4a5569f19aefa73356170c51735602311fcd1ea4
Muslim <magezamuslim40@gmail.com>
2023-03-22 02:11:27 +0600

    add -p: Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
-import "style/style.scss"
+import "./style/style.scss"
+import Home from "./pages/Home/Home";
+import Layout from "./Layout/Layout";
 function App() {
   return (
     <div className="App">
         <Routes>
-            <Route path={"/"}>
-                <Route path={"/"} element={"Home"}/>
+            <Route path={"/"} element={<Layout/>}>
+                <Route path={"/"} element={<Home/>}/>
             </Route>
         </Routes>
     </div>
---
+++ src/Layout/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            FOOTER
+        </footer>
+    );
+};
+
+export default Footer;
