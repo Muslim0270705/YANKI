51abe1c378bb67252f747b745713e23c82535a6c
Muslim <magezamuslim40@gmail.com>
2023-03-23 01:10:19 +0600

    add -p: HomeForm

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import New from "./New/New";
 import Category from "./Category/Category";
+import HomeForm from "./HomeForm/HomeForm";
 
 const Home = () => {
     return (
         <>
             <New/>
             <Category/>
+            <HomeForm/>
         </>
     );
 };
