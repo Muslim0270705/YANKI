9f4aa5cc6bcffe41e9fc860964828f503b5c5031
Muslim <magezamuslim40@gmail.com>
2023-03-22 15:48:11 +0600

    add -p: Category

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import New from "./New/New";
+import Category from "./Category/Category";
 
 const Home = () => {
     return (
         <>
             <New/>
+            <Category/>
         </>
     );
 };
